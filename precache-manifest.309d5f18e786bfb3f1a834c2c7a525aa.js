a6c2e704ba4b3378175a6882d99dda8bf73d5a2f
Febryardi13 <febryardiwork@gmail.com>
2020-07-04 08:32:08 +0700

    Updates

---
+++ precache-manifest.309d5f18e786bfb3f1a834c2c7a525aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30c998c38a18b78c93dfc7c0fe20445d",
+    "url": "/robocat-searchfunc/index.html"
+  },
+  {
+    "revision": "38f8b32c5e576a8a34b9",
+    "url": "/robocat-searchfunc/static/css/main.fb2116b5.chunk.css"
+  },
+  {
+    "revision": "747e42dfdc37503596ab",
+    "url": "/robocat-searchfunc/static/js/2.2f9d99e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robocat-searchfunc/static/js/2.2f9d99e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38f8b32c5e576a8a34b9",
+    "url": "/robocat-searchfunc/static/js/main.28fbca34.chunk.js"
+  },
+  {
+    "revision": "c312fe6ee492868e09ba",
+    "url": "/robocat-searchfunc/static/js/runtime-main.a257f370.js"
+  }
+]);
